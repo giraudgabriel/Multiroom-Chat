b81a28cbd4720bd255cde372129e9cf52dfee8c4
giraudgabriel <gabriel.giraud@fatec.sp.gov.br>
2019-11-03 16:57:17 -0300

    #FEATURE: Validação do campo de apelido no index.

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,18 @@
-module.exports.iniciaChat = (application, req, res) =>{
+module.exports.iniciaChat = (application, req, res) => {
+
+    let dados = req.body;
+
+    req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty()
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15)
+
+    let erros = req.validationErrors()
+
+    if (erros) {
+        res.render("index", {
+            validacao: erros,
+            apelido: req.body.apelido
+        })
+        return
+    }
     res.render("chat")
 }
